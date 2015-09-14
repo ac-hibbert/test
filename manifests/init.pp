0483aa668ce938e79a9b11aaff845f158bb2c57e
Andy Hibbert <anhibber@cisco.com>
2015-09-14 18:54:38 +0100

    Second release
    Change-Id: If90b85a0182c22751fd3dd288625ef07ce571bc4

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,5 +8,5 @@ class test (
     notify {"Parameter two: ${parameter_two}": }
     notify {"Templated parameter: ${templated_parameter}": }
     notify {"Details from CMS: Version: ${version} Purpose: ${purpose} Domain: ${domain} Env: ${env} Product: ${product} Domain: ${domain} Site: ${site}": }
-    notify {"This is the first release": }
+    notify {"This is the second release": }
 }
